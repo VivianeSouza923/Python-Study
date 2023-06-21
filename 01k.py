07dba13a4ca655cbe71fbb3949ae2b34ff97468e
VivianeSouza923 <viviane.souza05@aluno.ifce.edu.br>
2023-06-21 20:27:49 -0300

    update

---
+++ 01k.py
@@ -0,0 +1,51 @@
+# Kaggle - course :)
+
+# priting:
+
+print("Hello World")
+
+# ou
+
+print('Hello World')
+
+# Arithmetic:
+
+print(1+2)
+
+print(9-5)
+
+# You can control the order of operations in long calculations with parentheses.
+
+print(((1+3)*(9-2)/2)**2)
+
+# Multiply 3 by 2
+print(3*2)
+
+# Manipulating variables
+
+test_var = 4+5
+
+var = 3
+
+print(var)
+
+var = 100
+
+print(var)
+
+
+print(var)
+print(test_var)
+
+var = var + 3
+print(var)
+
+# Using multiple variables
+anos = 4
+dia_ano = 365
+hora_dia = 24
+min_hora = 60
+seg_min = 60
+
+total_seg = seg_min * min_hora * hora_dia * dia_ano * anos
+print(total_seg)
