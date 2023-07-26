fdbd707f8ad6d0c54368a15d6ef96c06a9104891
Vivi <70662490+VivianeSouza923@users.noreply.github.com>
2023-07-26 11:18:57 -0300

    Add files via upload
    PR "today's studies"

---
+++ tipos_de_dados.py
@@ -0,0 +1,22 @@
+''' tipos - os tipos servem para definir as características e comportamentos de um 
+valor (objeto) para o interpretador . 
+    Tipos em Py:
+        Texto - str;
+        Numerico - int, float, complex;
+        Sequência - list, tuple, range;
+        Mapa - dict;
+        Coleção - set, fronzenset;
+        Booleano - bool;
+        BInário - bytes, bytearray, memoryview.
+'''
+
+print(1 + 10)
+print(11 + 10 + 1000)
+
+print(1.5 + 1 + 0.5)
+
+print(True)
+
+print(False)
+
+print("Python")
