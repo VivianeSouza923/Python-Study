437a1e4680c33f88b4475268b0c11a605f7d1c1a
Vivi <70662490+VivianeSouza923@users.noreply.github.com>
2023-11-06 21:45:24 -0300

    Add files via upload

---
+++ gk1.py
@@ -0,0 +1,13 @@
+number = 9
+
+print(number)
+
+number = 4.5
+
+print(number)
+
+
+number = 'olá'
+
+print(number)
+
---
+++ gk2.py
@@ -0,0 +1,10 @@
+# criação de lista
+lista = []
+
+
+lista.append(1)
+lista.append(5.9)
+lista.append('ei')
+
+
+print(lista)
