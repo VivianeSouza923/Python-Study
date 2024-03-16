d7d1976dbc0f62ca9df271d136b69401cd8d40f4
Viviane Souza <viviane.souza05@aluno.ifce.edu.br>
2024-03-16 09:17:43 -0300

    operações matemáticas com Python e função Print. Fonte: Introdução à Programação com Python - Menezes, Nilo Ney Coutinho.

---
+++ livro_Menezes_Nilo_Ney_Coutinho_Introdução_a_Programacao_Com_Python/print('Olá, mundo').py
@@ -0,0 +1,4 @@
+# para dar um simples print em Python, eu só preciso usar o print e pronto.
+# posso usar aspas simples, aspas dupla. tanto faz.
+# não preciso finalizar meu print com ; ou qualquer outra coisa.
+print('Olá, mundo')
