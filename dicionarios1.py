103bd17dc9c6d444344886b344c08bb2ef7d3289
Viviane Souza <viviane.souza05@aluno.ifce.edu.br>
2024-07-22 17:16:24 -0300

    Estudo de funções aplicadas a dicionários com Python.

---
+++ dicionarios1.py
@@ -0,0 +1,49 @@
+'''
+    2. Dicionários:
+        ◦ Crie um dicionário que representa um carrinho de compras. Cada chave deve ser o nome do produto e o valor deve ser a 
+        quantidade. Implemente as funções adicionar_item, remover_item, calcular_total, e imprimir_carrinho.
+        ◦ Crie uma função que recebe um dicionário e retorna um novo dicionário com as chaves e valores invertidos.
+        ◦ Crie uma função que recebe um dicionário e retorna uma lista com os valores em ordem crescente.
+
+
+'''
+# criação do dicionário com suas devidas chaves e seus devidos valores
+carrinho_compras = {
+    'Arroz' : 3,
+    'Feijão' : 5,
+    'Macarrão' : 5,
+    'Café' : 5
+
+}
+
+def adicionar_item(chave, novo_item): # CRIAÇÃO DA FUNÇÃO QUE VAI ACRESCENTAR UM ITEM AO DICIONÁRIO
+    carrinho_compras[chave] = novo_item # adicionamos o noto item assim, CRIANDO UMA COLUNA
+    return carrinho_compras # retorna o novo carrinho de compras
+    
+    
+
+adicionar_item('Acúcar', 6) # adiciona açúcar
+print(carrinho_compras)
+
+
+def remover_item(chave): # função que vai remover o item que quero (aqui só preciso da chave mesmo)
+    del carrinho_compras[chave] # só deleta a coluna com o del
+    return carrinho_compras # retorna o carrinho de compras atualizado
+
+remover_item('Café') # remove café
+print(carrinho_compras)
+
+def calcular_total(valores): # função para calcular o montante das coisas 
+
+    valores = carrinho_compras.values() # pego somente os valores com a values(), as chaves n vem
+    valores_lista = list(valores) # transformo em lista esse conjunto de valores
+    soma_valores = sum(valores_lista)  # faço a soma dos itens dessa lista
+    print(soma_valores)
+   
+
+
+calcular_total(carrinho_compras) # imprime o montante total do carrinho de compras
+
+def imprimir_carrinho(dicio):
+    print() # CONTINUAAAAAR
+
